c634f3fc8c5a2fc8f4b0209eaa43866b3270e2a1
Nikos Niotis <niotisnikos@hotmail.com>
2020-11-02 19:35:14 +0200

    read folder names

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <dirent.h>
 
 int main() {
-    
+    DIR *dir_p;
+    struct dirent *folder;
+    int sites_counter = 0;
+    std::string* sites;
+    std::string input_dir = "./Datasets/2013_camera_specs/";
+    if ( (dir_p = opendir(input_dir.c_str())) == NULL ) {
+        perror("can't open the given directory");
+        exit(2);
+    } else {
+        while (folder = readdir(dir_p)) {
+            std::cout << "folder = " << folder->d_name << std::endl;
+            // for each json use name for constructor
+        }
+
+        (void) closedir (dir_p);
+    } 
 
     return 0;
 }
