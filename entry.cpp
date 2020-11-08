4a0639cf88cb1ac4175e937f469f50c81cc2ad45
ThomasPapathimiopoulos <thomaspapathimiopoylos@gmail.com>
2020-11-08 20:02:00 +0200

    added hashvalue in entries for avl insertion

--- entry.cpp
+++ entry.cpp
@@ -7,11 +7,13 @@ Entry::Entry(){
 
 Entry::Entry(std::string new_id, std::string new_page_title){
     id = new_id;           
-    page_title = new_page_title;  
-}
-
-Entry::Entry(std::string fullname){
-    //code to break string into id and page_title
+    page_title = new_page_title;
+    
+    int ascii_sum = 0;
+    for (int i = 0; i < page_title.size(); i++) { 
+        ascii_sum += (int)(page_title[i]); 
+    }
+    hashvalue = ascii_sum + std::stoi(id);
 }
 
 Entry::~Entry(){
@@ -22,8 +24,12 @@ std::string Entry::get_id(){
     return id;
 }
 
-std::string Entry::get_page_table(){
+std::string Entry::get_page_title(){
     return page_title;
+}
+
+int Entry::get_hashvalue(){
+    return hashvalue;
 }
 
 void Entry::print(){
--- entry.hpp
+++ entry.hpp
@@ -9,14 +9,15 @@ class Entry{
     private:
         std::string id;
         std::string page_title;
+        int hashvalue;
         // STRING LIST OF PRODUCTS;       
     public:
         Entry();                            //constructors
         Entry(std::string, std::string);
-        Entry(std::string);
         ~Entry();                           //destructor
         std::string get_id();               //getters
-        std::string get_page_table();
+        std::string get_page_title();
+        int get_hashvalue();
         void print();                       //misc   
 };
 
