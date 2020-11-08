6dbd95ddc0d26004b765b7236d933e8664b50641
ThomasPapathimiopoulos <thomaspapathimiopoylos@gmail.com>
2020-11-08 19:23:55 +0200

    begun implementing avl trees

---
+++ avl.hpp
@@ -0,0 +1,39 @@
+#ifndef _AVL_
+#define _AVL_
+
+#include <iostream>
+#include "entry.hpp"
+
+class Treenode{
+    private:
+        int height;
+        Entry entry;
+    public:
+        Treenode* left;
+        Treenode* right;
+
+        Treenode();
+        Treenode(Entry);
+        ~Treenode();
+        void set_height(int);
+        int get_height();
+        void updateHeight();
+};
+
+class AVL{
+    private:
+
+    public:
+        Treenode* root;
+        int size;
+
+        AVL();
+        ~AVL();
+        int get_size();
+        int get_balance(Treenode*);
+        Treenode* right_rotate(Treenode*);
+        Treenode* left_rotate(Treenode*);
+        Treenode* insert(Treenode*, Entry*);
+};
+
+#endif
