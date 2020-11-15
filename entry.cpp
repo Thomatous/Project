56a7eee4c96177f8a9009a1750683383df525cbf
Thomas Papathimiopoulos <thomaspapathimiopoylos@gmail.com>
2020-11-15 14:31:49 +0200

    commented entries

--- entry.cpp
+++ entry.cpp
@@ -2,9 +2,11 @@
 
 Entry::Entry(){
     id = "None";                //default value for not initialized id
-    page_title = "None";    //dedefault value for not initialized page_tiyle
+    page_title = "None";        //dedefault value for not initialized page_tiyle
 }
 
+//constructor for entries
+//assignes values given
 Entry::Entry(std::string new_page_title, std::string new_id){
     id = new_id;           
     page_title = new_page_title;
@@ -14,30 +16,37 @@ Entry::Entry(std::string new_page_title, std::string new_id){
     hashvalue = hash_value_calculator(new_page_title, new_id);
 }
 
+
+//entry destructor
 Entry::~Entry(){
     if(clique != NULL)
         delete clique;
 }
 
+//id getter
 std::string Entry::get_id(){
     return id;
 }
 
+//page title getter
 std::string Entry::get_page_title(){
     return page_title;
 }
 
+//hashvalue getter
 unsigned long long Entry::get_hashvalue(){
     return hashvalue;
 }
 
+//debug function prints entry strings
 void Entry::print(){
     std::cout << page_title << "//" << id << std::endl;
 }
 
+//merges the clique of this entry with the entry's given clique
 void Entry::merge(Entry *e){
     if(e->clique != clique){
-        clique->merge(e->clique);
-        clique->update_clique_ptrs(clique);
+        clique->merge(e->clique);           //call clique merrge
+        clique->update_clique_ptrs(clique); //make all entries in the clique point to this new merged one
     }
 }
