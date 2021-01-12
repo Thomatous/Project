b5362a42ac6d00638847bbc9298bb6fe36e2b35f
Thomas Papathimiopoulos <thomaspapathimiopoylos@gmail.com>
2021-01-12 22:22:27 +0200

    added two more job successors

--- job_scheduler.hpp
+++ job_scheduler.hpp
@@ -23,6 +23,44 @@ public:
     virtual ~Job() { }
 };
 
+class remove_not_best_Job : public Job{
+private:
+    SM *files;
+    Dict *best_words;
+
+public:
+    remove_not_best_Job(SM *n_files, Dict *n_best_words){
+        files = n_files;
+        best_words = n_best_words;
+    };
+    void run() override {
+        files->remove_not_best(best_words);
+    }
+};
+
+class sparse_matrix_Job : public Job{
+private:
+    SM *files;
+    Clique* list_of_entries;
+    Dict *all_words;
+    std::string *all_words_vector;
+    float *all_idf_vector;
+    float *all_tfidf_sum_vector;
+
+public:
+    sparse_matrix_Job(SM *n_files, Clique* n_list_of_entries, Dict *n_all_words, std::string *n_all_words_vector, float *n_all_idf_vector, float *n_all_tfidf_sum_vector){
+        files = n_files;
+        list_of_entries = n_list_of_entries;
+        all_words = n_all_words;
+        all_words_vector = n_all_words_vector;
+        all_idf_vector = n_all_idf_vector;
+        all_tfidf_sum_vector = n_all_tfidf_sum_vector;
+    };
+    void run() override {
+        files = new SM(list_of_entries, all_words_vector, all_tfidf_sum_vector, all_idf_vector, all_words);
+    }
+};
+
 class vectorify_Job : public Job{
 private:
     Dict *all_words;
@@ -40,7 +78,7 @@ public:
         all_tfidf_sum_vector = n_all_idf_vector;
         loc = n_loc;
         num_entries = n_num_entries;
-    };
+    }
     void run() override {
         all_words->vectorify(all_words->root, all_words_vector, all_idf_vector, all_tfidf_sum_vector, loc, num_entries);
     }
@@ -56,7 +94,7 @@ struct JobScheduler{
     JobScheduler* initialize_scheduler(int n_execution_threads){
         execution_threads = n_execution_threads;
         tids = new pthread_t[execution_threads];
-    };
+    }
 
     int submit_job(JobScheduler* sch, Job* j);
     int execute_all_jobs(JobScheduler* sch);
