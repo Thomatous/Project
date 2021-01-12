a84fb66a0991495d70cf27b049f882dccd73f055
Nikos Niotis <niotisnikos@hotmail.com>
2021-01-12 20:10:40 +0200

    implemented job Queue

---
+++ queue.hpp
@@ -0,0 +1,29 @@
+#ifndef _QUEUE_
+#define _QUEUE_
+
+#include <stdio.h>
+#include "job_scheduler.hpp"
+
+struct QueueNode {
+    Job* job;
+    QueueNode* next;
+
+    QueueNode(Job*);
+    ~QueueNode();
+};
+
+struct Queue {
+    QueueNode* head;
+    QueueNode* tail;
+    unsigned int size;
+
+    Queue();
+    ~Queue();
+    void push_back(Job*);
+    Job* pop();
+}
+
+
+
+
+#endif
