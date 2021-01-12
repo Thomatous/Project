078776b40cf06953d8599d5dfc329ff3468021dd
Thomas Papathimiopoulos <thomaspapathimiopoylos@gmail.com>
2021-01-12 18:59:04 +0200

    job class start

--- job_scheduler.hpp
+++ job_scheduler.hpp
@@ -4,12 +4,24 @@
 #include <iostream>
 #include <thread>
 
-struct Job{};
+class Job{
+public:
+    virtual void run() = 0;
+    virtual ~Job() { }
+};
+
+class DemoJob : public Job{
+public:
+    DemoJob(){};
+    void run() override {
+        //code here
+    }
+};
 
 struct JobScheduler{
     int execution_threads;  //number of execution threads
     //Queue* q;             // a queue that holds submitted jobs / tasks    !IMPLEMENT QUEUE
-    std::thread* tids;           // execution threads
+    std::thread* tids;      // execution threads
     // mutex, condition variable, ...
 
     JobScheduler* initialize_scheduler(int execution_threads);
